0c3ccbf3afb7f938b3d8ee95108ee90ab9efc1aa
MOvcharenko <maksym.ovcharenko@auk.edu.ua>
2026-03-23 23:39:28 +0200

    Fixed type import error.

--- src/components/deck/DeckListItem.tsx
+++ src/components/deck/DeckListItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { Deck } from '../types';
+import type { Deck } from '../../types';
 import { Link } from 'react-router-dom';
 import DeleteButton from '../common/DeleteButton';
 
