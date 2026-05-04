4caa492b508e0e5e96977f065f149774f727fb86
MOvcharenko <maksym.ovcharenko@auk.edu.ua>
2026-05-04 11:15:54 +0300

    Updated vite.config and github workflows.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/<repository-name>/', // Replace with your actual repository name for GitHub Pages
   server: {
     proxy: {
       '/api': {
